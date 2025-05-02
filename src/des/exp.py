31b6066b45fb9eec180a20614e30ff61f5a50849
Mithun Sivanesan <mithunsivanesan@gmail.com>
2025-05-02 13:23:48 +1000

    Added initial permutation

--- src/des/exp.py
+++ src/des/exp.py
@@ -1,4 +1,23 @@
+import random
+
 #This includs the DES function expansion permutation E and Inverse Expansion Permutation E^-1
 
-def expansion_permutation():
-    print("Expansion Permutation")
+def expansion_permutation(input: str):
+    inputList = [[None for i in range (8)] for j in range(8)]
+    evenList = [[None for i in range (8)] for j in range(4)]
+    oddList = evenList.deepcopy()
+    for i in range (64):
+        inputList[i//8][i%8] = int(input[i])
+    for i in range (8):
+        for j in range (8):
+            bit = inputList[i][j]
+    return inputList
+        
+
+if __name__ == "__main__":
+    input = ''.join(random.choice('01') for _ in range(64))  # Generate a 64-bit random binary string
+    print(input + "\n")
+    out = expansion_permutation(input)
+    for i in range (8):
+        for j in range (8):
+            print(out[i][j], end = '')
