f21bd6bfb7ea1d36009ac35acc6028b49a10819f
Mithun Sivanesan <mithunsivanesan@gmail.com>
2025-05-09 12:58:48 +1000

    Fixed pylint errors with __init__.py

---
+++ src/des/__init__.py
@@ -0,0 +1,11 @@
+"""
+DES module for COMP3260 Assessment 2.
+This module contains the functions for DES, such as:
+
+- Initial permutation (ip)
+- Expansion permutation (exp)
+- S boxes (sbox)
+- Permutation P (perms)
+"""
+
+from .exp import expansion_permutation
