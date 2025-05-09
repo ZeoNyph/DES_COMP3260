1f61bc7db21ab31ea10eb55fa4197ea61346c318
Mithun Sivanesan <mithunsivanesan@gmail.com>
2025-05-09 13:13:27 +1000

    Added skeleton keygen.py file

---
+++ src/des/keygen.py
@@ -0,0 +1,13 @@
+"""
+Round key generation for the DES module.
+
+Authors:
+Mithun Sivanesan (c3403606)
+"""
+
+
+def key_init(input_key: str) -> str:
+    """
+    Initializes key for round key generation
+    """
+    return input_key
