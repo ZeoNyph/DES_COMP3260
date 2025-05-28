2be68cf15e80bfa9f3d26be8541fc6f248f0143a
Mithun Sivanesan <mithunsivanesan@gmail.com>
2025-05-29 09:18:45 +1000

    added skeleton decrypt output logic for Chanelle to work with, added missing file.close()

--- src/main.py
+++ src/main.py
@@ -48,9 +48,13 @@ def main():
                     # DES 16 round comparison
                     for j in range(16):
                         # comparing P with P' with the same K
-                        avalanche[i][0].append(avalanche_comparison(round_c[i][0][j], round_c[i][1][j]))
+                        avalanche[i][0].append(
+                            avalanche_comparison(round_c[i][0][j], round_c[i][1][j])
+                        )
                         # comparing K and K' with the same P
-                        avalanche[i][1].append(avalanche_comparison(round_c[i][0][j], round_c[i][2][j]))
+                        avalanche[i][1].append(
+                            avalanche_comparison(round_c[i][0][j], round_c[i][2][j])
+                        )
                 end = time.perf_counter()
                 output(end - start, True)
                 print("Output saved to ./output.txt")
@@ -121,20 +125,24 @@ def encrypt(des_type: int, plaintext: str, key: str, ciphertext_type: int) -> st
         rh = "".join(ciphertext[32:])
         # separate instance of right half to keep original right half for next round
         f_rh = "".join(exp.expansion_permutation(ciphertext[32:]))
-        if des_type != 1: # skip xor with round key (DES1)
+        if des_type != 1:  # skip xor with round key (DES1)
             f_rh = utils.xor(f_rh, keys[i])
-        if des_type != 2: # do inv exp if DES2
+        if des_type != 2:  # do inv exp if DES2
             f_rh = sbox.sbox_function(f_rh)
         else:
             f_rh = "".join(exp.inverse_expansion_permutation(f_rh))
-        if des_type != 3: # skip P (DES3)
+        if des_type != 3:  # skip P (DES3)
             f_rh = "".join(perms.permutation(f_rh))
         f_rh = utils.xor(lh, f_rh)
         ciphertext = rh + f_rh
-        round_c[des_type][ciphertext_type].append(ciphertext) # for avalanche comparison later
+        round_c[des_type][ciphertext_type].append(
+            ciphertext
+        )  # for avalanche comparison later
     # 32-bit swap
     ciphertext = ciphertext[32:] + ciphertext[:32]
-    ciphertext = "".join(ip.initial_permutation(ciphertext, False)) # IP^-1 at end of round 16
+    ciphertext = "".join(
+        ip.initial_permutation(ciphertext, False)
+    )  # IP^-1 at end of round 16
     c[des_type].append(ciphertext)
 
 
@@ -147,46 +155,56 @@ def output(running_time: float, is_encrypt: bool):
         os.remove("output.txt")
 
     with open("output.txt", "x", encoding="utf-8") as file:
-        file.write(
-            "Avalanche Demonstration\n"
-            f"Plaintext P   : {p[0]}\n"
-            f"Plaintext P'  : {p[1]}\n"
-            f"Key K         : {k[0]}\n"
-            f"Key K'        : {k[1]}\n"
-            f"Total running time: {running_time:.3f} seconds\n\n"
-            f"P and P` under K\n"
-            f"Ciphertext C : {c[0][0]}\n"
-            f"Ciphertext C`: {c[0][1]}\n\n"
-            f"Round\t\t\tDES0\tDES1\tDES2\tDES3\n"
-        )
-        for i in range(17):
-            file.write(f"\t{i}\t\t\t {avalanche[0][0][i]}\t\t {avalanche[1][0][i]}\t\t {avalanche[2][0][i]}\t\t {avalanche[3][0][i]}\n")
-        file.write(
-            f"\nP under K and K`\n"
-            f"Ciphertext C : {c[0][0]}\n"
-            f"Ciphertext C`: {c[0][2]}\n\n"
-            f"Round\t\t\tDES0\tDES1\tDES2\tDES3\n"
-        )
-        for i in range(17):
-            file.write(f"\t{i}\t\t\t {avalanche[0][1][i]}\t\t {avalanche[1][1][i]}\t\t {avalanche[2][1][i]}\t\t {avalanche[3][1][i]}\n")
+        if is_encrypt:
+            file.write(
+                "Avalanche Demonstration\n"
+                f"Plaintext P   : {p[0]}\n"
+                f"Plaintext P'  : {p[1]}\n"
+                f"Key K         : {k[0]}\n"
+                f"Key K'        : {k[1]}\n"
+                f"Total running time: {running_time:.3f} seconds\n\n"
+                f"P and P` under K\n"
+                f"Ciphertext C : {c[0][0]}\n"
+                f"Ciphertext C`: {c[0][1]}\n\n"
+                f"Round\t\t\tDES0\tDES1\tDES2\tDES3\n"
+            )
+            for i in range(17):
+                file.write(
+                    f"\t{i}\t\t\t {avalanche[0][0][i]}\t\t {avalanche[1][0][i]}\t\t {avalanche[2][0][i]}\t\t {avalanche[3][0][i]}\n"
+                )
+            file.write(
+                f"\nP under K and K`\n"
+                f"Ciphertext C : {c[0][0]}\n"
+                f"Ciphertext C`: {c[0][2]}\n\n"
+                f"Round\t\t\tDES0\tDES1\tDES2\tDES3\n"
+            )
+            for i in range(17):
+                file.write(
+                    f"\t{i}\t\t\t {avalanche[0][1][i]}\t\t {avalanche[1][1][i]}\t\t {avalanche[2][1][i]}\t\t {avalanche[3][1][i]}\n"
+                )
+        else:
+            print("DO HERE")
+        file.close()
+
 
 def avalanche_comparison(p1: str, p2: str) -> int:
     """
-    This method compares the original plaintext string with the 
+    This method compares the original plaintext string with the
         cipherext string produced after each round.
-    
+
     It converts the strings into lists, then loops through the length of the original plaintext.
-    While looping to checks whether the current value in the plaintext and ciphertext does 
-        not match. 
-    If it doesn't then it adds one to the counter, otherwise it just continues. 
+    While looping to checks whether the current value in the plaintext and ciphertext does
+        not match.
+    If it doesn't then it adds one to the counter, otherwise it just continues.
     """
     counter = 0
     p1_list = list(p1)
     p2_list = list(p2)
     for i in range(64):
         if p1_list[i] != p2_list[i]:
-            counter+=1
+            counter += 1
     return counter
+
 
 if __name__ == "__main__":
     main()
